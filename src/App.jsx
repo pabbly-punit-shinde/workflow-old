95951c4c8cf744ad93a5987d43f38086749bd6d1
PunitShinde <punit.shinde@pabbly.com>
2024-10-25 17:07:30 +0530

    icon to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useLayoutEffect } from "react";
+import { Icon } from "@iconify-icon/react";
 import {
   ReactFlow,
   ReactFlowProvider,
@@ -10,6 +11,7 @@ import {
   Controls,
   MiniMap,
   Background,
+  ControlButton,
 } from "@xyflow/react";
 import ELK from "elkjs/lib/elk.bundled.js";
 import CustomNode from "./CustomNodes.jsx"; // Import the custom node component
@@ -115,14 +117,35 @@ function LayoutFlow() {
         draggable={true}
       >
         <Panel position="top-right">
-          <button onClick={() => onLayout({ direction: "DOWN" })}>
-            Auto Align Vertically
-          </button>
-          <button onClick={() => onLayout({ direction: "RIGHT" })}>
-            Auto Align Horizontally
-          </button>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "5px",
+            }}
+          >
+            <h6>Alignment Buttons</h6>
+            <button onClick={() => onLayout({ direction: "DOWN" })}>
+              <Icon
+                icon="ph:tree-structure-fill"
+                style={{ transform: "rotate(90deg)" }}
+              />
+            </button>
+            <button onClick={() => onLayout({ direction: "RIGHT" })}>
+              <Icon icon="ph:tree-structure-fill" />
+            </button>
+          </div>
           {/* Buttons to change edge type */}
-          <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "5px",
+            }}
+          >
+            <h6>Select Edge Type</h6>
             <button onClick={() => setEdgeType("straight")}>Straight</button>
             <button onClick={() => setEdgeType("step")}>Step</button>
             <button onClick={() => setEdgeType("smoothstep")}>
@@ -131,6 +154,7 @@ function LayoutFlow() {
             <button onClick={() => setEdgeType("bezier")}>Bezier</button>
           </div>
         </Panel>
+        <ControlButton />
         <Background bgColor="#f3faf7" />
         <Controls />
         <MiniMap />
